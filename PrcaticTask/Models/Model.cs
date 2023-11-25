ebab609068f1b3516d87d04b59fccd6332c98bd7
Марат Музафаров <mmuzafarov74@gmail.com>
2023-11-25 18:34:03 +0500

    Добавьте файлы проекта.

---
+++ PrcaticTask/Models/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace PrcaticTask.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
